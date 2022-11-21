702e4e796c2ec4c377e57f01b6a89ca002c9f4a0
Ulisses Albuquerque <iamurise@gmail.com>
2022-11-21 07:30:09 +0000

    add fetch markers

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,28 @@
-let map;
+async function getCenters() {
+  const res = await fetch(
+    "https://3000-googlemaps-jssamples-wy5atajly1u.ws-us77.gitpod.io/bloodCenters"
+  );
+  return res.json();
+}
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+function coordinatesToPosition(coordinates) {
+  return { lat: coordinates[0], lng: coordinates[1] };
+}
+
+async function initMap() {
+  const brazilCenter = { lat: -14.4086569, lng: -51.31668 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: brazilCenter,
+  });
+
+  const centers = await getCenters();
+  centers.map((center) => {
+    const position = coordinatesToPosition(center.point.coordinates);
+    new google.maps.Marker({
+      map: map,
+      position,
+    });
   });
 }
 

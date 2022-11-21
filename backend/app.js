6945c7dba79de17e4654a584d08c47943d2f85f8
Brunoklb <70604350+Brunoklb@users.noreply.github.com>
2022-11-21 20:28:23 -0300

    use lat and lng in req body

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import cors from "cors";
+import { db } from "./db.js";
 import express from "express";
 import { PORT } from "./env.js";
 import { BloodCenter } from "./bloodCenter.js";
@@ -6,7 +7,7 @@ import { BloodCenter } from "./bloodCenter.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/bloodCenters", async (req, res) => {
   const centers = await BloodCenter.findAll();
@@ -15,21 +16,17 @@ app.get("/bloodCenters", async (req, res) => {
 
 app.post("/bloodCenters", async (req, res) => {
   const { lat, lng } = req.body;
-  console.log(req.body)
   if (!lat) {
-    return res
-      .status(400)
-      .send("missing lat attribute on request body");
+    return res.status(400).send("missing lat attribute on request body");
   }
   if (!lng) {
-    return res
-      .status(400)
-      .send("missing lng attribute on request body");
+    return res.status(400).send("missing lng attribute on request body");
   }
   let center = new BloodCenter({
     ...req.body,
     point: {
-      type: "Point", coordinates: [lat, lng]
+      type: "Point",
+      coordinates: [lat, lng],
     },
   });
 
@@ -50,6 +47,8 @@ app.use((error, req, res, next) => {
 });
 
 async function run() {
+  db.authenticate();
+  db.sync();
   app.listen(PORT, () => {
     console.log("server running on http://localhost:" + PORT);
   });

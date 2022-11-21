da3e228e98a5934c6055a8283583a14a5486974c
Ulisses Albuquerque <iamurise@gmail.com>
2022-11-21 13:47:17 +0000

    add details expand

--- frontend/src/displayMap.js
+++ frontend/src/displayMap.js
@@ -3,7 +3,7 @@ import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import { coordinatesToPosition } from "./coordinatesToPosition";
 import { api } from "./api";
 
-export function DisplayMapImplementation() {
+export function DisplayMapImplementation({ setSelected }) {
   const [centers, setCenters] = useState([]);
   const brazilCenter = { lat: -14.4086569, lng: -51.31668 };
   const { isLoaded } = useJsApiLoader({
@@ -41,7 +41,13 @@ export function DisplayMapImplementation() {
     >
       {centers.map((center) => {
         const position = coordinatesToPosition(center.point.coordinates);
-        return <Marker key={center.id} position={position}></Marker>;
+        return (
+          <Marker
+            key={center.id}
+            position={position}
+            onClick={() => { setSelected(center) }}
+          ></Marker>
+        );
       })}
     </GoogleMap>
   ) : (
--- frontend/src/listPage.js
+++ frontend/src/listPage.js
@@ -1,11 +1,14 @@
-import { AddForm } from "./addForm";
+import { useState } from "react";
 import { DisplayMap } from "./displayMap";
+import { CenterDetails } from "./centerDetails";
 
 export function ListPage() {
+  const [selected, setSelected] = useState();
+
   return (
     <div className="flex flex-row h-screen">
-      <DisplayMap></DisplayMap>
-      <AddForm></AddForm>
+      <DisplayMap setSelected={setSelected}></DisplayMap>
+      <CenterDetails center={selected}></CenterDetails>
     </div>
   );
 }

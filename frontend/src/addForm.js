b0aebc226f2b22dd864c120cc2096bec4464eea2
Brunoklb <70604350+Brunoklb@users.noreply.github.com>
2022-11-21 21:12:01 -0300

    finish

--- frontend/src/addForm.js
+++ frontend/src/addForm.js
@@ -1,19 +1,24 @@
 import { api } from "./api";
 import { RequiredIndicator } from "./requiredIndicator";
 import { useForm } from "react-hook-form";
-import { baseURL } from "./env";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
 
 export function AddForm({ position }) {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+  const [success, setSuccess] = useState(false);
 
   async function onSubmit(data) {
     data.lat = position.lat();
     data.lng = position.lng();
     await api.post("/bloodCenters", data);
+    reset();
+    setSuccess(true);
   }
 
   return (
-    <div className="p-4 flex flex-col gap-2 shadow z-10 h-full justify-center">
+    <div className="p-4 flex flex-col gap-2 z-10 h-full justify-center">
+      {success && <Navigate to="/" replace={true} />}
       <h1 className="text-lg">Adicionar Hemonúcleo</h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
@@ -33,11 +38,7 @@ export function AddForm({ position }) {
             <input name="lng" type="hidden" value={position.lng()}></input>
           </>
         )}
-        <button
-          type="submit"
-          disabled={!position}
-          className="text-white bg-blue-500 rounded-lg p-1 mt-4 active:scale-[95%] transition-all hover:bg-blue-600 duration-100 disabled:saturate-[.2] disabled:hover:bg-blue-500"
-        >
+        <button type="submit" disabled={!position} className="common-button">
           Enviar
         </button>
         {!position && <div>Clique no mapa para selecionar o local</div>}
